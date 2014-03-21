43b469e8782c3ee23030944c89f483355ac38a5e
Jan Drábek <me@jandrabek.cz>
2014-03-22 00:23:10 +0100

    Refactored updatedb procedure.

--- app/updatedb.php
+++ app/updatedb.php
@@ -2,5 +2,5 @@
 include_once __DIR__ . "/bootstrap.php";
 
 // Check if migrations should be done
-$dm = new DatabaseMigrator($container->getService('nette.database.default'), __DIR__ . '/../resources/migrations/');
+$dm = new DatabaseMigrator($container->getByType('Nette\\Database\\Context'), __DIR__ . '/../resources/migrations/');
 $dm->migrate();
--- app/utils/DatabaseMigrator.php
+++ app/utils/DatabaseMigrator.php
@@ -1,5 +1,6 @@
 <?php
 use Nette\Database\Connection;
+use Nette\Database\Context;
 use Nette\Database\SqlLiteral;
 use Nette\Object;
 use Nette\Utils\Strings;
@@ -13,16 +14,17 @@ final class DatabaseMigrator extends Object {
 
 	const MIGRATION_TABLE_NAME = "meta_migrations";
 
-	/** @var Connection */
-	private $connection;
+	/** @var Context */
+	private $context;
 	private $path;
 
 	/**
 	 * @param Connection $connection Database connection
 	 * @param $path Real absolute path where SQL scripts are stored
 	 */
-	public function __construct(Connection $connection, $path) {
-		$this->connection = $connection;
+	public function __construct(Context $context, $path) {
+		$this->context = $context;
+
 		$this->path = $path;
 	}
 
@@ -41,9 +43,9 @@ final class DatabaseMigrator extends Object {
 	}
 
 	private function checkMigrationTable() {
-		$rows = $this->connection->query('SHOW TABLES LIKE ?', self::MIGRATION_TABLE_NAME);
-		if($rows->rowCount() == 0) {
-			$this->connection->query('
+		$rows = $this->context->query('SHOW TABLES LIKE ?', self::MIGRATION_TABLE_NAME);
+		if($rows->getRowCount() == 0) {
+			$this->context->query('
 				CREATE TABLE ? (file VARCHAR(255) NOT NULL, UNIQUE(file))
 			', new SqlLiteral(self::MIGRATION_TABLE_NAME));
 		}
@@ -55,7 +57,7 @@ final class DatabaseMigrator extends Object {
 		$this->checkMigrationTable();
 		$files = $this->getFiles();
 		foreach($files as $file) {
-			$processed = $this->connection->query('SELECT COUNT(*) FROM ? WHERE file = ?', new SqlLiteral(self::MIGRATION_TABLE_NAME), $file)->fetchAll();
+			$processed = $this->context->query('SELECT COUNT(*) FROM ? WHERE file = ?', new SqlLiteral(self::MIGRATION_TABLE_NAME), $file)->fetchAll();
 			if($processed[0][0] > 0) {
 				echo "Skipping already processed file: ". $file."\n";
 				continue;
@@ -64,18 +66,19 @@ final class DatabaseMigrator extends Object {
 			$content = file_get_contents($this->path . '/' . $file);
 			$content .= " INSERT INTO ". self::MIGRATION_TABLE_NAME . " (file) VALUES ('" . $file ."');";
 			try {
-				$this->connection->beginTransaction();
-				$this->connection->exec($content);
-				$this->connection->commit();
+				$this->context->beginTransaction();
+				$this->context->exec($content);
+				$this->context->commit();
+
 			} catch (PDOException $ex) {
 				echo "FAIL: ". $file . "\n";
 				echo "Error message: " . $ex->getMessage() . "\n";
 				echo $ex->getTraceAsString();
-				$this->connection->rollBack();
+				$this->context->rollBack();
 			} catch (Exception $ex) {
 				echo "FAIL: ". $file . "\n";
 				echo $ex->getTraceAsString();
-				$this->connection->rollBack();
+				$this->context->rollBack();
 			}
 		}
 	}
