41433775dcd7d1b3ec1277bf731c7e2ad1c8f59a
Jan Drábek <jan.drabek@trigama.eu>
2017-09-29 19:19:22 +0200

    Fix disableLinks on magnificPopup

--- js/disableLinks.js
+++ js/disableLinks.js
@@ -1,9 +1,11 @@
 $(function(){
-    // Disabling all navigable things in preview
-    $(document).on('click', 'a,button,input[type=submit],input[type=button]', function (e) {
-        alert('Odkazy a tlačítka nejsou v náhledu funkční.');
-        e.preventBubble();
-        e.preventDefault();
-        return false;
-    });
+	// Disabling all navigable things in preview
+	$(document).on('click', 'a,button,input[type=submit],input[type=button]', function (e) {
+		alert('Odkazy a tlačítka nejsou v náhledu funkční.');
+		return false;
+	});
+	$('a,button,input[type=submit],input[type=button]').click(function () {
+		alert('Odkazy a tlačítka nejsou v náhledu funkční.');
+		return false;
+	})
 });
