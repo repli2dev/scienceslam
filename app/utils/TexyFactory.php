8b6c6e0780e5b948995a1d56b328c7dd24c7ff3e
Jan Drábek <jan.drabek@trigama.eu>
2017-09-30 12:28:18 +0200

    Fix indent and add Texy comment syntax to help

--- app/utils/TexyFactory.php
+++ app/utils/TexyFactory.php
@@ -17,15 +17,15 @@ class TexyFactory
 	/** @var Texy */
 	private $texyWithoutSnippets;
 
-    /** @var Snippet */
-    private $snippetDAO;
+	/** @var Snippet */
+	private $snippetDAO;
 
-    public function __construct(Snippet $snippetDAO)
-    {
-        $this->snippetDAO = $snippetDAO;
-    }
+	public function __construct(Snippet $snippetDAO)
+	{
+		$this->snippetDAO = $snippetDAO;
+	}
 
-    public static function getSyntaxHelp($id = null)
+	public static function getSyntaxHelp($id = null)
 	{
 		$wrapper = Html::el();
 		$button = Html::el('a')->setText('Nápověda formátování');
@@ -57,13 +57,17 @@ Nečíslovaný seznam se dělá takto:
 
 \---
 
+/---comment
+Zakomentováno, nebude ve výstupu.
+\---
+
 [* /images/blocks/preview01-spectrometer.jpg .(alternativní text)[image_right clear] *]')
 		);
 		$el->add(Html::el()->setText('Dostupné formátovací třídy:'));
 		$el->add(Html::el('pre')->setText('div .[wide-text-block]'));
 		$el->add(Html::el('pre')->setText('div .[wrapped]'));
 		$el->add(Html::el('pre')->setText('.[spaced]'));
-        $el->add(Html::el('pre')->setText('.[no-margin]'));
+		$el->add(Html::el('pre')->setText('.[no-margin]'));
 		$el->add(Html::el('br'));
 		$el->add(
 			Html::el('a')->href('https://texy.info/cs/syntax')->setHtml('<br>')->setText('Detailní dokumentace syntaxe')
@@ -80,50 +84,50 @@ Nečíslovaný seznam se dělá takto:
 		if (!$this->texy) {
 			$this->texy = new Texy();
 			// Add support for including snippets
-            $this->texy->registerBlockPattern(
-                function (BlockParser $parser, array $matches, $name)
-                {
-                    list(, $mKey) = $matches;
-                    $texy = $this->getTexyWithoutSnippets();
+			$this->texy->registerBlockPattern(
+				function (BlockParser $parser, array $matches, $name)
+				{
+					list(, $mKey) = $matches;
+					$texy = $this->getTexyWithoutSnippets();
 
-                    $el = new HtmlElement('div');
-                    $el->attrs['class'] = 'texy-snippet-' . $mKey;
-                    $snippet = $this->snippetDAO->getByKey($mKey);
-                    if ($snippet) {
-                        $temp = $texy->process($snippet->content);
-                        $el->parseBlock($parser->getTexy(), "/---html\n" . $temp . "\n\----\n");
-                    }
-                    return $el;
-                },
-                '#^\.snippet\n(.+)$#m', // block patterns must be multiline and line-anchored
-                'snippetSyntax'
-            );
+					$el = new HtmlElement('div');
+					$el->attrs['class'] = 'texy-snippet-' . $mKey;
+					$snippet = $this->snippetDAO->getByKey($mKey);
+					if ($snippet) {
+						$temp = $texy->process($snippet->content);
+						$el->parseBlock($parser->getTexy(), "/---html\n" . $temp . "\n\----\n");
+					}
+					return $el;
+				},
+				'#^\.snippet\n(.+)$#m', // block patterns must be multiline and line-anchored
+				'snippetSyntax'
+			);
 		}
 		return $this->texy;
 	}
 
 	public function getTexyWithoutSnippets()
-    {
-        if (!$this->texyWithoutSnippets) {
-            $this->texyWithoutSnippets = new Texy();
-            // Eat all .snippets to be eaten
-            $this->texyWithoutSnippets->registerBlockPattern(
-                function (BlockParser $parser, array $matches, $name)
-                {
-                    return null;
-                },
-                '#^\.snippet\n(.+)$#m', // block patterns must be multiline and line-anchored
-                'snippetSyntax'
-            );
-        }
-        return $this->texyWithoutSnippets;
-    }
+	{
+		if (!$this->texyWithoutSnippets) {
+			$this->texyWithoutSnippets = new Texy();
+			// Eat all .snippets to be eaten
+			$this->texyWithoutSnippets->registerBlockPattern(
+				function (BlockParser $parser, array $matches, $name)
+				{
+					return null;
+				},
+				'#^\.snippet\n(.+)$#m', // block patterns must be multiline and line-anchored
+				'snippetSyntax'
+			);
+		}
+		return $this->texyWithoutSnippets;
+	}
 
 	public function install(Engine $engine)
 	{
 		$temp = new TexyMacro($engine, $this->getTexy());
 		$temp->install();
 		$engine->addFilter('texy', [$this->getTexy(), 'process']);
-        $engine->addFilter('texyWithoutSnippets', [$this->getTexyWithoutSnippets(), 'process']);
+		$engine->addFilter('texyWithoutSnippets', [$this->getTexyWithoutSnippets(), 'process']);
 	}
 }
