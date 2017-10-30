82517b78a7b61eabaf95efbaeec1914d71bfe3b7
Martin Ukrop <mukrop@mail.muni.cz>
2017-10-30 20:10:48 +0100

    Delete files with private configuration

--- tour/admin/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Database
-define("DBDSN", "mysql:host=localhost;dbname=scienceslam;charset=utf8"); //PDO connection details
-define("DBUSER", "scienceslam"); //DB username
-define("DBPASS", "sciencedbpassword1520268913254"); //DB password
-?>
